8c0bddcacf2ebdc7683683d476b872c14db58c4d
Raymondl177 <raymondl177@nycstudents.net>
2026-03-13 12:30:47 -0400

    sw

---
+++ app/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const crime = ref([]);
+
+async function getCrimeData() {
+  try {
+    const response = await fetch('https://data.cityofnewyork.us/api/v3/views/qgea-i56i/query.json');
+    data = await response.json();
+    crime.value = data.results
+  } catch (error) {
+    console.error('Error fetching crime data:', error);
+  }
+}
+
+getCrimeData();
+</script>
+
+<style scoped>
+
+</style>
